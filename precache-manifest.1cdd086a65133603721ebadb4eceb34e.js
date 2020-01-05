31503d97710f422290ce43c805cdad046c2400e2
lironezra <lironezra1@gmail.com>
2020-01-05 10:24:37 +0200

    Updates

---
+++ precache-manifest.1cdd086a65133603721ebadb4eceb34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbe7763ff22fd2a0e0dd739b4145c03",
+    "url": "/FastRider/index.html"
+  },
+  {
+    "revision": "7994bd177ed868c61617",
+    "url": "/FastRider/static/css/main.a627d30c.chunk.css"
+  },
+  {
+    "revision": "42772ee0ffb1035b6dc4",
+    "url": "/FastRider/static/js/2.0f3db674.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/FastRider/static/js/2.0f3db674.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7994bd177ed868c61617",
+    "url": "/FastRider/static/js/main.93c4af59.chunk.js"
+  },
+  {
+    "revision": "829ff0c0236c5a54f525",
+    "url": "/FastRider/static/js/runtime-main.6c1afadd.js"
+  }
+]);
